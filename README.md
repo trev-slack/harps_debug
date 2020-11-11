b41865fcc3fd533c895f843b3de1614bccfeea9f
trev-slack <trevorpslack@gmail.com>
2020-11-11 15:55:46 -0700

    individual windows

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Debug Window for HARPS Sim
 Echos the rostopics published by the harps sim for debug purposes.
 ## Install 
-Clone the repo and make the scripts executable with chmod +x. Additionally install GNU parallel with "sudo apt-get install parallel"
+Clone the repo and make the scripts executable with chmod +x.
 ## Running
 Run "./harps_debug.sh" to view the various rostopics.
--- harps_debug.sh
+++ harps_debug.sh
@@ -1,15 +1,10 @@
 #!/bin/bash 
-tp1="/Drone1/pose"
-tp2="/Drone1/image_raw"
-tp3="/GoalReached"
-tp4="/rosout"
-tp5="/PullAnswer"
-tp6="/Sketch"
-tp7="/Drone1/Goal"
-tp8="/AcceptQuest"
-tp9="/Target/car"
-tp10="/rosout_agg"
-tp11="/Pull"
-tp12="/Camera_Num"
-tp13="/Obs"
-parallel -u ::: './harps_debug_subscriber.sh $tp1' './harps_debug_subscriber.sh $tp2' './harps_debug_subscriber.sh $tp3' './harps_debug_subscriber.sh $tp4' './harps_debug_subscriber.sh $tp5' './harps_debug_subscriber.sh $tp6' './harps_debug_subscriber.sh $tp7' './harps_debug_subscriber.sh $tp8' './harps_debug_subscriber.sh $tp9' './harps_debug_subscriber.sh $tp10' './harps_debug_subscriber.sh $tp11' './harps_debug_subscriber.sh $tp12' './harps_debug_subscriber.sh $tp13'
+tp=("/Drone1/pose" "/Drone1/image_raw" "/GoalReached" "/rosout" "/PullAnswer" "/Sketch" "/Drone1/Goal" "/AcceptQuest" "/Target/car" "/rosout_agg" "/Pull" "/Camera_Num" "/Obs")
+
+for i in {1..13}
+do
+gnome-terminal -- "'./harps_debug_subscriber.sh ${tp[$i]}'" 
+done
+#xterm -title "App 1" -e "mycommand; mysecondcommand" 
+#all in one window
+#parallel -u ::: './harps_debug_subscriber.sh $tp1' './harps_debug_subscriber.sh $tp2' './harps_debug_subscriber.sh $tp3' './harps_debug_subscriber.sh $tp4' './harps_debug_subscriber.sh $tp5' './harps_debug_subscriber.sh $tp6' './harps_debug_subscriber.sh $tp7' './harps_debug_subscriber.sh $tp8' './harps_debug_subscriber.sh $tp9' './harps_debug_subscriber.sh $tp10' './harps_debug_subscriber.sh $tp11' './harps_debug_subscriber.sh $tp12' './harps_debug_subscriber.sh $tp13'
